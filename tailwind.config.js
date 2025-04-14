24003464fd9a087331444a6f2dc29aab0f796163
sazzad <sazzadador@gmail.com>
2025-04-14 18:35:15 +0600

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {
-        
+        fontFamily: {
+          inter: ['Inter', 'sans-serif'],
+          playfair: ['Playfair Display', 'serif'],
+        }
       },
     },
  
